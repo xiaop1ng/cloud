737ab77fca322233845f6e0f38943818f5842cd4
xiaop1ng <784516419@qq.com>
2020-05-20 20:27:12 +0800

    nacos

--- server-nacos/src/main/java/com/hlang/App.java
+++ server-nacos/src/main/java/com/hlang/App.java
@@ -1,14 +1,19 @@
 package com.hlang;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(App.class, args);
+        String userName = applicationContext.getEnvironment().getProperty("user.name");
         String userAge = applicationContext.getEnvironment().getProperty("user.age");
-        System.err.println("age: "+userAge);
+        System.err.println("name: " + userName + "age: "+userAge);
     }
+
 }
