9f83d8f89265bf90701584c956d7f09da5678f82
xiaop1ng <784516419@qq.com>
2020-08-26 09:51:36 +0800

    hanlp

--- server-api/src/main/java/com/hlang/api/CfgApi.java
+++ server-api/src/main/java/com/hlang/api/CfgApi.java
@@ -1,5 +1,11 @@
 package com.hlang.api;
 
+import com.hankcs.hanlp.HanLP;
+import com.hankcs.hanlp.seg.common.Term;
+import com.xiaoping.base.BaseController;
+import com.xiaoping.base.impl.BaseBizController;
+import com.xiaoping.pojo.Rs;
+import com.xiaoping.utils.DataRow;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
@@ -10,8 +16,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.xml.crypto.Data;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
 @RestController
-public class CfgApi {
+public class CfgApi extends BaseBizController {
+
+    private Logger logger = Logger.getLogger("CfgApi");
 
     @Value("${user.name}")
     private String name;
@@ -35,6 +48,25 @@ public class CfgApi {
         String url = String.format("http://%s:%s/echo/%s",serviceInstance.getHost(),serviceInstance.getPort(),name);
         System.out.println("request url:"+url);
         return restTemplate.getForObject(url,String.class);
+    }
+
+    @GetMapping("/hanlp")
+    public Rs call() {
+        String text = this.getStringParam("text");
+        List<Term> segment = HanLP.segment(text);
+        List<DataRow> list = new ArrayList<>();
+        logger.info("[size]" + segment.size());
+        List<String> list2 = HanLP.extractKeyword(text, 1);
+
+
+        segment.forEach(T->{
+            DataRow data = new DataRow();
+            data.set("word", T.word);
+            data.set("nature", String.valueOf(T.nature));
+            data.set("offset", T.offset);
+            list.add(data);
+        });
+        return Rs.ok(list);
     }
 
 }
